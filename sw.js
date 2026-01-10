2c5617b85206835134708dfab87b570397ac8979
joseperusinijp-beep <joseperusini.jp@gmail.com>
2026-01-10 16:11:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
 /* sw.js — Service Worker simple para Trapial VIT */
-const CACHE_NAME = 'trapial-vit-v2.3 Final';
+const CACHE_NAME = 'trapial-vit-v2.4 Final';
 const PRECACHE_URLS = [
   './',
   './index.html',
@@ -41,6 +41,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
