3499fadb09cbcd4d91e2f82c9ef97fd836d3207d
joseperusinijp-beep <joseperusini.jp@gmail.com>
2026-01-11 18:18:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,17 @@
 
 // sw.js
-const CACHE_NAME = 'trapial-vit-v11'; // subir versión en cada despliegue
-const ASSETS = ['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-
-self.addEventListener('install',(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))))});
-self.addEventListener('fetch',(e)=>{
-  const req=e.request,accept=req.headers.get('accept')||'';
-  if(req.mode==='navigate'||accept.includes('text/html')){
-    e.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE_NAME).then(c=>c.put(req,copy));return res}).catch(()=>caches.match(req)));
-    return;
-  }
-  e.respondWith(caches.match(req).then(c=>c||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE_NAME).then(c=>c.put(req,copy));return res})));
-});
-
-
-
-
-
+const CACHE_NAME = 'trapial-vit-v1';
+const ASSETS = [
+  './',                // raíz del proyecto
+  './index.html',
+  './site.webmanifest',
+  './icons/favicon.ico',
+  './icons/favicon-32x32.png',
+  './icons/favicon-16x16.png',
+  './icons/apple-touch-icon.png',
+  './icons/android-chrome-192x192.png',
+  './icons/android-chrome-512x512.png'
+];
 
 
 
