bf1acf3c95d6d0242233498360d515b225f6b71b
joseperusinijp-beep <joseperusini.jp@gmail.com>
2026-01-13 09:27:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 
+// sw.js
 const CACHE_NAME = 'trapial-vit-v3';
 const ASSETS = [
   './',
-  './index.html',
+  './index (2).html',
   './manifest.json',
   './assets/chevron-hallmark.svg',
   './icons/favicon.ico',
@@ -12,3 +13,22 @@ const ASSETS = [
   './icons/android-chrome-192x192.png',
   './icons/android-chrome-512x512.png'
 ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
